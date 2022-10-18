07708be2673456d7a594ce2ffd760bf71b72cf9c
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-10-18 11:23:03 +0200

    update add spinners and change layout of rows

--- app/src/main/java/com/example/projecttech_v4/MainActivity.java
+++ app/src/main/java/com/example/projecttech_v4/MainActivity.java
@@ -62,26 +62,26 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner spinner1 = findViewById(R.id.spinner1_operator);
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource
-                (getApplicationContext(), R.array.operator, android.R.layout.simple_spinner_item);
+                (this, R.array.operator, android.R.layout.simple_spinner_item);
 
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
-        spinner1.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-//
-//        Spinner spinner2 = findViewById(R.id.spinner2_data);
-//        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource
-//                (this, R.array.operator, android.R.layout.simple_spinner_item);
-//
-//        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner2.setAdapter(adapter1);
+//        spinner1.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        Spinner spinner2 = findViewById(R.id.spinner2_data);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource
+                (this, R.array.data, android.R.layout.simple_spinner_item);
+
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
 //        spinner2.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-//
-//        Spinner spinner3 = findViewById(R.id.spinner3_godzina);
-//        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource
-//                (this, R.array.operator, android.R.layout.simple_spinner_item);
-//
-//        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner3.setAdapter(adapter1);
+
+        Spinner spinner3 = findViewById(R.id.spinner3_godzina);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource
+                (this, R.array.godzina_zmiany, android.R.layout.simple_spinner_item);
+
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
 //        spinner3.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
 
     }
